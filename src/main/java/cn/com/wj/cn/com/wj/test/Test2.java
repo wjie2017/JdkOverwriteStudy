baae9229207fc61afbecd5659d8da0600db2cf3f
wangjie <wangjie2@nantian.com.cn>
2019-10-15 10:31:10 +0800

    idea测试git使用

---
+++ src/main/java/cn/com/wj/cn/com/wj/test/Test2.java
@@ -0,0 +1,19 @@
+package cn.com.wj.cn.com.wj.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * todo
+ *
+ * @author wangjie
+ * @version V1.0
+ * @date 2019/10/15
+ */
+public class Test2 {
+    protected static final Logger logger = LoggerFactory.getLogger(Test2.class);
+
+    public static void main(String[] args) {
+             logger.info("");
+    }
+}
